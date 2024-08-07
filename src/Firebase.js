ae4354efe8535a2ae2f1a39663b166e99a37e5e1
Suraj Godse <godsesuraj2140@gmail.com>
2024-08-07 16:14:56 -0500

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBlYTB89nO0dJcpW1820FByZtM8jpZNKLY",
-  authDomain: "auth-app-12july24.firebaseapp.com",
-  projectId: "auth-app-12july24",
-  storageBucket: "auth-app-12july24.appspot.com",
-  messagingSenderId: "689921384255",
-  appId: "1:689921384255:web:fa7257f761fad1866cfd8a"
+  apiKey: "*",
+  authDomain: "*",
+  projectId: "*",
+  storageBucket: "*",
+  messagingSenderId: "*",
+  appId: "*"
 };
 
 const app = initializeApp(firebaseConfig);
